3e2644f2ae4d0b3d4645c4778152cead17cd027b
Liran Tal <liran.tal@gmail.com>
2017-09-22 23:04:21 +0300

    feat(jwt): tidying up strategy definition

--- server/modules/users/server/config/strategies/jwt.js
+++ server/modules/users/server/config/strategies/jwt.js
@@ -7,22 +7,24 @@ const UserService = require('../../services/user.service')
 const JwtStrategy = passportJwt.Strategy
 const ExtractJwt = passportJwt.ExtractJwt
 
-module.exports = function (config) {
-  passport.use(new JwtStrategy({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    secretOrKey: config.jwt.secret
-  }, async (jwtPayload, done) => {
-    try {
-      const user = await UserService.getUserDeserializedById(jwtPayload.id)
-      if (user) {
-        return done(null, user)
-      } else {
-        return done(null, false, {
-          message: 'Incorrect token'
-        })
-      }
-    } catch (err) {
-      return done(err)
+async function verifyCallback(jwtPayload, done) {
+  try {
+    const user = await UserService.getUserDeserializedById(jwtPayload.id)
+    if (user) {
+      return done(null, user)
+    } else {
+      return done(null, false)
     }
-  }))
+  } catch (err) {
+    return done(err)
+  }
+}
+
+module.exports = function (config) {
+  const jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken()
+  const secretOrKey = config.jwt.secret
+
+  const strategy = new JwtStrategy({jwtFromRequest, secretOrKey}, verifyCallback)
+
+  passport.use(strategy)
 }
