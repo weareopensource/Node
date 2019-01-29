409f43ec6e01ed0f38920de4b10e4de5878f8cbe
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-01-29 11:41:54 +0100

    :shirt: quick lint & clean to standardize with tasks tests

--- modules/users/routes/users.server.routes.js
+++ modules/users/routes/users.server.routes.js
@@ -5,18 +5,17 @@ const passport = require('passport');
 const path = require('path');
 
 const model = require(path.resolve('./lib/middlewares/model'));
-const schema = require('../models/user.server.schema');
+const usersSchema = require('../models/user.server.schema');
 
 
 module.exports = (app) => {
-  // User Routes
   const users = require('../controllers/users.server.controller');
 
   // Setting up the users profile api
   app.route('/api/users/me').get(passport.authenticate('jwt'), users.me);
-  app.route('/api/users').put(passport.authenticate('jwt'), model.isValid(schema.User), users.update);
+  app.route('/api/users').put(passport.authenticate('jwt'), model.isValid(usersSchema.User), users.update);
   app.route('/api/users/accounts').delete(users.removeOAuthProvider);
-  app.route('/api/users/accounts').post(model.isValid(schema.User), users.addOAuthProviderUserProfile);
+  app.route('/api/users/accounts').post(model.isValid(usersSchema.User), users.addOAuthProviderUserProfile);
   app.route('/api/users/password').post(passport.authenticate('jwt'), users.changePassword);
   app.route('/api/users/picture').post(passport.authenticate('jwt'), users.changeProfilePicture);
 
