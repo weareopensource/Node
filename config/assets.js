fc1797b40abe4191848d530418a2fb80918b4f82
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-02-12 10:47:52 +0100

    :hammer: rework models path

--- config/assets.js
+++ config/assets.js
@@ -2,8 +2,8 @@
 module.exports = {
   gulpConfig: ['gulpfile.js'],
   allJS: ['server.js', 'config/**/*.js', 'modules/*/**/*.js'],
-  mongooseModels: 'modules/*/models/mongoose/*.js',
-  sequelizeModels: 'modules/*/models/sequelize/*.js',
+  mongooseModels: 'modules/*/models/*.mongoose.js',
+  sequelizeModels: 'modules/*/models/*.sequelize.js',
   routes: ['modules/!(core)/routes/*.js', 'modules/core/routes/*.js'],
   // sockets: 'modules/*/sockets/*.js',
   config: ['modules/*/config/*.js'],
