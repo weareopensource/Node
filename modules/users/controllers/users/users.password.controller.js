20eb60fb17d0621dbef56a901568e8aa837c957c
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-03-27 14:17:20 +0100

    refactor(users): clean password code :shirt:

--- modules/users/controllers/users/users.password.controller.js
+++ modules/users/controllers/users/users.password.controller.js
@@ -19,21 +19,19 @@ const config = require(path.resolve('./config'));
  */
 exports.forgot = async (req, res) => {
   let user;
-  let token;
   let send;
 
   // check input
   if (!req.body.email) return responses.error(res, 422, 'Mail field must not be blank')();
 
+  // get user generate and add token
   try {
-    // get user
     user = await UserService.get({ email: req.body.email });
     if (!user) return responses.error(res, 400, 'No account with that email has been found')();
     if (user.provider !== 'local') return responses.error(res, 400, `It seems like you signed up using your ${user.provider} account`)();
-    // update user with recover token
-    const payload = { exp: Date.now() + 3600000 };
+
     const edit = {
-      resetPasswordToken: jwt.sign(payload, configuration.jwt.secret, { algorithm: 'HS256' }),
+      resetPasswordToken: jwt.sign({ exp: Date.now() + 3600000 }, configuration.jwt.secret, { algorithm: 'HS256' }),
       resetPasswordExpires: Date.now() + 3600000,
     };
     user = await UserService.update(user, edit, 'recover');
@@ -41,7 +39,8 @@ exports.forgot = async (req, res) => {
     responses.error(res, 422, errors.getMessage(err))(err);
   }
 
-  const template = mail.generateTemplate(res, 'reset-password-email', user.displayName, token);
+  // send mail
+  const template = mail.generateTemplate(res, 'reset-password-email', user.displayName, user.resetPasswordToken);
   if (template) send = mail.sendMail(config.mailer.from, user.email, 'Password Reset', template);
   if (!template || !send) return responses.error(res, 400, 'Failure sending email')();
   responses.success(res, 'An email has been sent to the provided email with further instructions.')();
@@ -73,31 +72,29 @@ exports.validateResetToken = async (req, res) => {
  * @param {Object} res - Express response object
  */
 exports.reset = async (req, res) => {
-  // Init Variables
-  const newPassword = req.body.newPassword;
-  const resetPasswordToken = req.body.token;
   let user;
   let send;
 
+  // check input
+  if (!req.body.token || !req.body.newPassword) return responses.error(res, 422, 'Password or Token fields must not be blank')();
+
+  // get user by token, update with new password, login again
   try {
-    // Hash new password
-    const password = await UserService.hashPassword(newPassword);
-    // get user
     user = await UserService.search({
-      resetPasswordToken,
+      resetPasswordToken: req.body.token,
       resetPasswordExpires: {
         $gt: Date.now(),
       },
     });
-    if (!user) return responses.error(res, 400, 'Password reset token is invalid or has expired.')();
-    // update user
+    if (user.length !== 1) return responses.error(res, 400, 'Password reset token is invalid or has expired.')();
+
     const edit = {
-      password,
+      password: await UserService.hashPassword(req.body.newPassword),
       resetPasswordToken: undefined,
       resetPasswordExpires: undefined,
     };
-    user = await UserService.update(user, edit, 'recover');
-    // reset login
+    user = await UserService.update(user[0], edit, 'recover');
+
     req.login(user, (errLogin) => {
       if (errLogin) return responses.error(res, 400, errors.getMessage(errLogin))(errLogin);
       user.password = undefined;
@@ -108,6 +105,7 @@ exports.reset = async (req, res) => {
     responses.error(res, 422, errors.getMessage(err))(err);
   }
 
+  // send mail
   const template = mail.generateTemplate(res, 'reset-password-confirm-email', user.displayName);
   if (template) send = mail.sendMail(config.mailer.from, user.email, 'Your password has been changed', template);
   if (!template || !send) return responses.error(res, 400, 'Failure sending email')();
@@ -118,27 +116,26 @@ exports.reset = async (req, res) => {
  * Change Password
  */
 exports.changePassword = async (req, res) => {
-  // Init Variables
-  const passwordDetails = req.body;
   let user;
   let password;
 
+  // check input
   if (!req.user) return responses.error(res, 401, 'User is not signed in')();
-  if (!passwordDetails.newPassword) return responses.error(res, 422, 'Please provide a new password')();
+  if (!req.body.newPassword) return responses.error(res, 422, 'Please provide a new password')();
 
+  // get user, check password, update user, login again
   try {
-    // get user
     user = await UserService.get({ id: req.user.id });
     if (!user) return responses.error(res, 400, 'User is not found')();
-    // check password
-    if (await UserService.comparePassword(passwordDetails.currentPassword, user.password)) {
-      if (passwordDetails.newPassword === passwordDetails.verifyPassword) {
-        password = passwordDetails.newPassword;
+
+    if (await UserService.comparePassword(req.body.currentPassword, user.password)) {
+      if (req.body.newPassword === req.body.verifyPassword) {
+        password = req.body.newPassword;
       } else return responses.error(res, 422, 'Passwords do not match')();
     } else return responses.error(res, 422, 'Current password is incorrect')();
-    // update user
+
     user = await UserService.update(user, { password }, 'recover');
-    // reset login
+
     req.login(user, (errLogin) => {
       if (errLogin) return responses.error(res, 400, errors.getMessage(errLogin))();
       responses.success(res, 'Password changed successfully')();
