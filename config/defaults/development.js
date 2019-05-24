01d946301cf1cbc3dfe592c85dc2619bfee676bb
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-05-24 22:22:59 +0200

    docs(wiki): add auth informations :book:

--- config/defaults/development.js
+++ config/defaults/development.js
@@ -102,8 +102,10 @@ module.exports = {
   zxcvbn: {
     minimumScore: 3,
   },
+  // jwt is for token authentification
   jwt: {
-    secret: 'test',
+    secret: 'test', // secret for hash
+    expiresIn: 7 * 24 * 60 * 60, // token expire in x sec
   },
   mailer: {
     from: 'WAOS_NODE_mailer_from',
