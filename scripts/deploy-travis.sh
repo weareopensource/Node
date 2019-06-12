6c48adb3c667b66a1a099fade917034d884f47a5
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-06-12 11:28:32 +0200

    ci(travis): review var names 👷🏼

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -4,13 +4,13 @@ eval "$(ssh-agent -s)" # Start ssh-agent cache
 chmod 600 $TRAVIS_BUILD_DIR/.travis/deploy_key # Allow read access to the private key
 ssh-add $TRAVIS_BUILD_DIR/.travis/deploy_key # Add the private key to SSH
 
-ssh -p $SSH_PORT $SSH_USER@$SSH_SERVER -o StrictHostKeyChecking=no "$( cat <<EOT
-  echo "$(date -u) Deploy '${DEPLOY_PROJECT}'"  >> ./deploy.log
-  cd '${DEPLOY_PATH}'
+ssh -p $sshPort $sshUser@$sshHost -o StrictHostKeyChecking=no "$( cat <<EOT
+  echo "$(date -u) Deploy '${depProject}'"  >> ./deploy.log
+  cd '${depPath}'
   git pull
   npm install
-  pm2 stop '${DEPLOY_PROJECT}'
-  pm2 delete '${DEPLOY_PROJECT}'
+  pm2 stop '${depProject}'
+  pm2 delete '${depProject}'
   pm2 flush
   npm run prod
   exit
