06970b93b9e66b0e05dca26d4f8bca7e8eb47873
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-06-12 15:21:34 +0200

    ci(travis): add port & node option 👷🏼

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -5,14 +5,16 @@ chmod 600 $TRAVIS_BUILD_DIR/deploy_key_node # Allow read access to the private k
 ssh-add $TRAVIS_BUILD_DIR/deploy_key_node # Add the private key to SSH
 
 ssh -p $sshPort $sshUser@$sshHost -o StrictHostKeyChecking=no "$( cat <<EOT
-  echo "$(date -u) Deploy '${depProject}'"  >> ./deploy.log
+  source .zshrc
   cd '${depPath}'
-  git pull
-  npm install
   pm2 stop '${depProject}'
   pm2 delete '${depProject}'
   pm2 flush
-  npm run prod
+  nvm install '${depNode}'
+  git pull
+  npm i
+  WAOS_NODE_port='${depPort}' npm run prod
+  echo "$(date -u) Deploy ${depProject} on node $(node -v) with npm $(npm -v)"  >> ./deploy.log
   exit
 EOT
 )"
