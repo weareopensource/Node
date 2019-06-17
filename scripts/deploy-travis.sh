10a8cca52196f74dca0ef463fe59c8f1da62393a
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2019-06-17 11:16:05 +0200

    ci(travis): update script 👷🏼

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -5,12 +5,14 @@ chmod 600 $TRAVIS_BUILD_DIR/deploy_key_node # Allow read access to the private k
 ssh-add $TRAVIS_BUILD_DIR/deploy_key_node # Add the private key to SSH
 
 ssh -p $sshPort $sshUser@$sshHost -o StrictHostKeyChecking=no "$( cat <<EOT
+  echo "$(date -u) Deploy ${depProject} on node $(node -v) with npm $(npm -v)"  >> ./deploy.log
   source .zshrc
   cd '${depPath}'
   pm2 stop '${depProject}'
   pm2 delete '${depProject}'
   pm2 flush
   nvm install '${depNode}'
+  nvm use '${depNode}'
   git pull
   npm i
   WAOS_NODE_port='${depPort}' npm run prod
