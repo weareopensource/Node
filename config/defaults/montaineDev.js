3b8ffc7311936e2511ea1fe5e6bb1a097aac7b8c
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2020-03-06 19:22:37 +0100

    feat(config): init montaine configuration ✨

---
+++ config/defaults/montaineDev.js
@@ -0,0 +1,17 @@
+const _ = require('lodash');
+const defaultConfig = require('./development');
+
+module.exports = _.merge(defaultConfig, {
+  app: {
+    title: 'Montaine Node (Dev Environment)',
+    contact: 'brisorgueilp@gmail.com',
+  },
+  port: 3011,
+  db: {
+    uri: 'mongodb://localhost/LouNodeDev',
+    debug: false,
+  },
+  cors: {
+    origin: ['http://localhost:8011'],
+  },
+});
