ac735cb5bfbc6ddcfc0d500ef6c546ed328842ab
Pierre Brisorgueil <pierrebrisorgueil@me.com>
2022-06-28 08:55:53 +0200

    Fix passport session (#1947)
    * docs(changelog): clean 📝
    
    * fix(global): remove sessions since new passport to respect stateless 🐛

--- modules/users/routes/admin.routes.js
+++ modules/users/routes/admin.routes.js
@@ -15,17 +15,17 @@ module.exports = (app) => {
   app.route('/api/users/stats').all(policy.isAllowed).get(admin.stats);
 
   // Users
-  app.route('/api/users').get(passport.authenticate('jwt'), policy.isAllowed, admin.list); // list
+  app.route('/api/users').get(passport.authenticate('jwt', { session: false }), policy.isAllowed, admin.list); // list
 
   // Users page
-  app.route('/api/users/page/:userPage').get(passport.authenticate('jwt'), policy.isAllowed, admin.list); // list
+  app.route('/api/users/page/:userPage').get(passport.authenticate('jwt', { session: false }), policy.isAllowed, admin.list); // list
 
   // Single user routes
   app
     .route('/api/users/:userId')
     .get(admin.get) // get
-    .put(passport.authenticate('jwt'), policy.isAllowed, admin.update) // update
-    .delete(passport.authenticate('jwt'), policy.isAllowed, admin.delete); // delete
+    .put(passport.authenticate('jwt', { session: false }), policy.isAllowed, admin.update) // update
+    .delete(passport.authenticate('jwt', { session: false }), policy.isAllowed, admin.delete); // delete
 
   // Finish by binding the user middleware
   app.param('userId', admin.userByID);
